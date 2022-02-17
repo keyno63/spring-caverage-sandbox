2166e9d91ee8a211fb2cee6f4622ffb2a1ee6640
Keyno <kofujiw@gmail.com>
2022-02-18 06:07:54 +0900

    Initial commit

---
+++ src/main/java/tokyo/maigoname/caver/CaverApplication.java
@@ -0,0 +1,13 @@
+package tokyo.maigoname.caver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tokyo/maigoname/caver/CaverApplicationTests.java
@@ -0,0 +1,13 @@
+package tokyo.maigoname.caver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
